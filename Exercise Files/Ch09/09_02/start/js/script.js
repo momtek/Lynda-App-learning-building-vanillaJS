3e557e0db496cac2853b27990bc7b50b2407e554
momtek <momtek.barry@gmail.com>
2018-11-19 20:48:23 +0100

    finilize the exercises

--- Exercise Files/Ch09/09_02/start/js/script.js
+++ Exercise Files/Ch09/09_02/start/js/script.js
@@ -2,7 +2,7 @@
 
 (function() {
 	const url = "http://api.openweathermap.org/data/2.5/weather?q=";
-	const apiKey = "APIKEY"; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
+	const apiKey = "9228d88b6deb368a5cfe5288cf52893b"; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
 	const activities = {
 		teamIn: ['basketball','hockey','volleyball'],
 		teamOutWarm: ['softball/baseball','football/soccer','American football','rowing','tennis','volleyball','ultimate frisbee','rugby'],
